4e88d400f73d7ee86e76818831284106a139ddff
Keiran Raine <kr2@sanger.ac.uk>
2017-11-22 09:49:07 +0000

    js lint corrections

--- js/View/Track/ProportionalWiggle/ProportionalXYPlot.js
+++ js/View/Track/ProportionalWiggle/ProportionalXYPlot.js
@@ -8,19 +8,10 @@ define([
   'JBrowse/View/Track/_YScaleMixin',
   'JBrowse/Util',
   'JBrowse/View/Track/Wiggle/_Scale'
-],
-function(
-  declare,
-  array,
-  lang,
-  Color,
-  on,
-  WiggleBase,
-  YScaleMixin,
-  Util,
-  Scale
-) {
-  return declare([WiggleBase, YScaleMixin], {
+], function(declare, array, lang, Color, on, WiggleBase, YScaleMixin, Util, Scale) {
+  return declare([
+    WiggleBase, YScaleMixin
+  ], {
     _defaultConfig: function() {
       return Util.deepUpdate(lang.clone(this.inherited(arguments)), {
         autoscale: 'local',
@@ -41,14 +32,11 @@ function(
           var scaling = new Scale(this.config, stats);
 
           // bump minDisplayed to 0 if it is within 0.5% of it
-          if (Math.abs(scaling.min / scaling.max) < 0.005) scaling.min = 0;
+          if (Math.abs(scaling.min / scaling.max) < 0.005)
+            scaling.min = 0;
 
           // update our track y-scale to reflect it
-          this.makeYScale({
-            fixBounds: true,
-            min: scaling.min,
-            max: scaling.max
-          });
+          this.makeYScale({fixBounds: true, min: scaling.min, max: scaling.max});
 
           // and finally adjust the scaling to match the ruler's scale rounding
           scaling.min = this.ruler.scaler.bounds.lower;
@@ -76,17 +64,17 @@ function(
 
       var lineColor;
       var urlTmp = thisB.config.urlTemplates;
-      for(var i=0; i<urlTmp.length; i++) {
-        if(urlTmp[i].name === 'counts') {
+      for (var i = 0; i < urlTmp.length; i++) {
+        if (urlTmp[i].name === 'counts') {
           lineColor = urlTmp[i].color;
           break;
         }
       }
 
-      if(scale < 1) {
+      if (scale < 1) {
         var background = new Image();
         background.src = bConfig.baseUrl + 'plugins/ProportionalMultiBw/img/zoom_in.png';
-        context.drawImage(background,0,parseInt((canvasHeight/2)-10));
+        context.drawImage(background, 0, parseInt((canvasHeight / 2) - 10));
       }
 
       var refBases = [];
@@ -95,19 +83,22 @@ function(
       var ratio = Util.getResolution(context, bConfig.highResolutionMode);
       var toY = lang.hitch(this, function(val) {
         var norm = 0;
-        if(val > 0) norm =  dataScale.normalize(val);
+        if (val > 0)
+          norm = dataScale.normalize(val);
         return canvasHeight * (1 - norm) / ratio;
       });
 
-      if(scale < 1) {
+      if (scale < 1) {
         var lastXY = {};
         // iterate over pixels (scale pixels per base)
         array.forEach(pixels, function(p, i) {
           var counts = 0;
           // iterate over stores
           array.forEach(p, function(s) {
-            if (!s) return;
-            if(s.feat.get('source') !== 'counts') return;
+            if (!s)
+              return;
+            if (s.feat.get('source') !== 'counts')
+              return;
             counts = s.score;
           }, this);
 
@@ -116,107 +107,118 @@ function(
           context.strokeStyle = lineColor;
 
           context.beginPath();
-          if(lastXY.hasOwnProperty('x')) {
+          if (lastXY.hasOwnProperty('x')) {
             context.moveTo(lastXY.x, lastXY.y);
             context.lineTo(i, score);
             context.stroke();
           }
-          lastXY = {x: i, y: score};
+          lastXY = {
+            x: i,
+            y: score
+          };
 
         }, this);
-      }
-      else {
+      } else {
         thisB.browser.getStore('refseqs', function(refSeqStore) {
-          refSeqStore.getReferenceSequence(
-              { ref: thisB.refSeq.name, start: leftBase, end: rightBase},
-              dojo.hitch(this, function(seq) {
-                refBases = seq.split('');
-                // the moved bit starts here
-                var maxRefFrac = thisB.config.maxRefFrac;
+          refSeqStore.getReferenceSequence({
+            ref: thisB.refSeq.name,
+            start: leftBase,
+            end: rightBase
+          }, dojo.hitch(this, function(seq) {
+            refBases = seq.split('');
+            // the moved bit starts here
+            var maxRefFrac = thisB.config.maxRefFrac;
 
-                var colors = {};
-                var templates = thisB.config.urlTemplates;
-                for(var i=0; i<templates.length; i++) {
-                  colors[templates[i].name] = templates[i].color;
+            var colors = {};
+            var templates = thisB.config.urlTemplates;
+            for (var i = 0; i < templates.length; i++) {
+              colors[templates[i].name] = templates[i].color;
+            }
+
+            var lastXY = {};
+            // iterate over pixels (scale pixels per base)
+            array.forEach(pixels, function(p, i) {
+              var stack = [];
+              var counts = 0;
+              // iterate over stores
+              array.forEach(p, function(s) {
+                if (!s)
+                  return;
+
+                var f = s.feat;
+                var source = f.get('source');
+                var score = s.score;
+
+                if (source === 'counts') {
+                  counts = score;
+                } else if (score > 0) {
+                  stack.push({
+                    'allele': source,
+                    'raw': score,
+                    'refBase': refBases[f.get('start') - leftBase]
+                  });
+                }
+              }, this);
+
+              var len = stack.length;
+              if (stack.length > 0) {
+                // test if we should show based on ref base divergence
+                var show = false;
+                if (matchRef) {
+                  for (var j = 0; j < stack.length; j++) {
+                    if (stack[j].allele === stack[j].refBase && stack[j].raw <= maxRefFrac) {
+                      show = true;
+                      break;
+                    }
+                  }
+                } else {
+                  show = true;
                 }
 
-                var lastXY = {};
-                // iterate over pixels (scale pixels per base)
-                array.forEach(pixels, function(p, i) {
-                  var stack = [];
-                  var counts = 0;
-                  // iterate over stores
-                  array.forEach(p, function(s) {
-                    if (!s) return;
-
-                    var f = s.feat;
-                    var source = f.get('source');
-                    var score = s.score;
-
-                    if(source === 'counts') {
-                      counts = score;
+                if (show) {
+                  // sort them by the raw value desc (or allele asc if equal)
+                  stack.sort(function(a, b) {
+                    if (a.raw === b.raw) {
+                      if (a.allele < b.allele)
+                        return -1;
+                      if (a.allele > b.allele)
+                        return 1;
                     }
-                    else if (score > 0) {
-                      stack.push({'allele': source, 'raw': score, 'refBase': refBases[f.get('start') - leftBase]});
-                    }
-                  }, this);
+                    return b.raw - a.raw;
+                  });
 
-                  var len = stack.length;
-                  if(stack.length > 0) {
-                    // test if we should show based on ref base divergence
-                    var show = false;
-                    if(matchRef) {
-                      for(var j=0; j<stack.length; j++) {
-                        if(stack[j].allele === stack[j].refBase && stack[j].raw <= maxRefFrac) {
-                          show = true;
-                          break;
-                        }
-                      }
-                    }
-                    else {
-                      show = true;
-                    }
-
-                    if(show) {
-                      // sort them by the raw value desc (or allele asc if equal)
-                      stack.sort(function(a,b) {
-                        if(a.raw === b.raw) {
-                          if(a.allele < b.allele) return -1;
-                          if(a.allele > b.allele) return 1;
-                        }
-                        return b.raw - a.raw;
-                      });
-
-                      var height = canvasHeight;
-                      for (var k = 0; k < len; k++) {
-                        context.fillStyle = colors[stack[k].allele];
-                        thisB._fillRectMod(context, i, 0, 1, height);
-                        height -= Math.round(canvasHeight * stack[k].raw);
-                      }
-                    }
+                  var height = canvasHeight;
+                  for (var k = 0; k < len; k++) {
+                    context.fillStyle = colors[stack[k].allele];
+                    thisB._fillRectMod(context, i, 0, 1, height);
+                    height -= Math.round(canvasHeight * stack[k].raw);
                   }
+                }
+              }
 
-                  // line drawing
-                  var score = toY(counts);
-                  context.strokeStyle = lineColor;
+              // line drawing
+              var score = toY(counts);
+              context.strokeStyle = lineColor;
 
-                  context.beginPath();
-                  if(lastXY.hasOwnProperty('x')) {
-                    context.moveTo(lastXY.x, lastXY.y);
-                    context.lineTo(i, score);
-                    context.stroke();
-                  }
-                  lastXY = {x: i, y: score};
+              context.beginPath();
+              if (lastXY.hasOwnProperty('x')) {
+                context.moveTo(lastXY.x, lastXY.y);
+                context.lineTo(i, score);
+                context.stroke();
+              }
+              lastXY = {
+                x: i,
+                y: score
+              };
 
-                }, this);
-              }),
-              // end callback
-              function() {},
-              // error callback
-              dojo.hitch(this, function() {
-                console.log(this);
-              })
-          );
+            }, this);
+          }),
+          // end callback
+          function() {},
+          // error callback
+          dojo.hitch(this, function() {
+            console.log(this);
+          }));
         });
       }
 
